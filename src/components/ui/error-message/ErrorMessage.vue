081c0bd086b42e0eeb295bac07867ea9da2b2df0
Danilo Colasso <danilo.colasso@gmail.com>
2025-01-26 13:04:59 -0300

    feat(ui): add Input, Card, Checkbox, and Button components

---
+++ src/components/ui/error-message/ErrorMessage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div v-if="error" class="text-xs text-red-500">
+    {{ error }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  error: {
+    type: String,
+    required: false,
+    default: null,
+  },
+})
+</script>
